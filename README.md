1c6a7760733dcbcaa458a811a699768db3518f45
unknown <petrashko.andrey@yandex.ru>
2025-05-18 02:57:16 +0500

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/andrey-petrashko/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,31 @@
+const content = document.querySelector('.content');
+const places__list = content.querySelector('.places__list');
+
+function addCard(link, name) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = link ;
+  cardElement.querySelector('.card__title').textContent = name ;
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function () {
+  const listItem = deleteButton.closest('.places__item');
+  deleteCard(listItem);
+  }); 
+  places__list.append(cardElement);
+}
+
+function deleteCard(listItem) {
+  listItem.remove();
+}
+
+initialCards.forEach(function (elements) {
+  const link = elements.link;
+  const name = elements.name;
+  addCard(link, name);
+});
+
+
+
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
