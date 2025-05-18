d274d5232d464423dc5ba6000ce3219bfc5a1aaa
unknown <petrashko.andrey@yandex.ru>
2025-05-18 14:56:23 +0500

    1st fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,33 @@
 const content = document.querySelector('.content');
 const places__list = content.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-function addCard(link, name) {
-  const cardTemplate = document.querySelector('#card-template').content;
+function createCard(cardData) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = link ;
-  cardElement.querySelector('.card__title').textContent = name ;
+  cardElement.querySelector('.card__image').src = cardData.link ;
+  cardElement.querySelector('.card__image').alt = ('Фото '+ cardData.name);
+  cardElement.querySelector('.card__title').textContent = cardData.name;
   const deleteButton = cardElement.querySelector('.card__delete-button');
   deleteButton.addEventListener('click', function () {
   const listItem = deleteButton.closest('.places__item');
   deleteCard(listItem);
   }); 
-  places__list.append(cardElement);
+  return (cardElement);
+}
+
+function renderCard() {
+  initialCards.forEach(function (elements) {
+    const cardData = elements;
+    const cardElement = createCard(cardData); 
+    places__list.prepend(cardElement);
+  });
 }
 
 function deleteCard(listItem) {
   listItem.remove();
 }
 
-initialCards.forEach(function (elements) {
-  const link = elements.link;
-  const name = elements.name;
-  addCard(link, name);
-});
-
-
+renderCard();
 
 // @todo: Темплейт карточки
 
