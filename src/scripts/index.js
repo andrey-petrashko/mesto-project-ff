b70d3e4da476865e071e6a65a37ce396dfe9d6d8
unknown <petrashko.andrey@yandex.ru>
2025-07-01 00:53:08 +0500

    7sprint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,41 @@
 import "../pages/index.css";
 import { createCard, handleDelete, handleLike } from "./card.js";
-import { initialCards } from "./cards.js";
+
+
 import {
   prepareAnimation,
   handleOverlayClick,
   openPopup,
   closePopup,
 } from "./modals.js";
+import { enableValidation, clearValidation } from "./validation.js";
+
+const validationConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
 
 const content = document.querySelector(".content");
 export const places__list = content.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 const profileDescription = document.querySelector(".profile__description");
 const popupProfileEdit = document.querySelector(".popup_type_edit");
+const profileImage = document.querySelector(".profile__image");
+const formPopupProfileEdit = popupProfileEdit.querySelector(".popup__form");
+
 const profileDescriptionInput = popupProfileEdit.querySelector(
   ".popup__input_type_description"
 );
 
 const popupNewCard = document.querySelector(".popup_type_new-card");
-const profileNameInput = popupProfileEdit.querySelector(".popup__input_type_name");
+const newCardForm = popupNewCard.querySelector(".popup__form");
+const profileNameInput = popupProfileEdit.querySelector(
+  ".popup__input_type_name"
+);
 const profileTitle = document.querySelector(".profile__title");
 const formNewPlace = document.forms["new-place"];
 const formEditProfile = document.forms["edit-profile"];
@@ -33,8 +50,34 @@ const imagePopupCaption = largeImagePopup.querySelector(".popup__caption");
 
 formEditProfile.addEventListener("submit", function (event) {
   event.preventDefault();
-  profileTitle.textContent = profileNameInput.value;
-  profileDescription.textContent = profileDescriptionInput.value;
+
+  fetch('https://nomoreparties.co/v1/wff-cohort-41/users/me', {
+    method: 'PATCH',
+    headers: {
+      authorization: '8d0b0fef-7fc0-4f9e-a81c-c75c92bbe32f',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: profileNameInput.value,
+      about: profileDescriptionInput.value
+    })
+  })
+    .then(function(res) {
+      return res.json();
+    })
+    .then((data) => {
+      profileTitle.textContent = data.name;
+      profileDescription.textContent = data.about;
+      console.log(data);
+    });
+
+
+
+
+
+
+ // profileTitle.textContent = profileNameInput.value;
+ // profileDescription.textContent = profileDescriptionInput.value;
   closePopup(popupProfileEdit);
 });
 
@@ -55,7 +98,7 @@ formNewPlace.addEventListener("submit", function (event) {
   closePopup(popupNewCard);
 });
 
-function renderCard() {
+function renderCard(initialCards) {
   initialCards.forEach(function (elements) {
     const cardData = elements;
     const cardElement = createCard(
@@ -69,7 +112,7 @@ function renderCard() {
   });
 }
 
-renderCard();
+
 const popups = document.querySelectorAll(".popup");
 prepareAnimation(popups);
 
@@ -81,6 +124,7 @@ const profileEditButton = document.querySelector(".profile__edit-button");
 profileEditButton.addEventListener("click", openPopupProfileEdit);
 
 function openPopupProfileEdit() {
+  clearValidation(formPopupProfileEdit, validationConfig);
   profileNameInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
   openPopup(popupProfileEdit);
@@ -90,6 +134,8 @@ const newCardButton = document.querySelector(".profile__add-button");
 newCardButton.addEventListener("click", openPopupNewCard);
 
 function openPopupNewCard() {
+  clearValidation(newCardForm, validationConfig);
+  newCardForm.reset();
   openPopup(popupNewCard);
 }
 
@@ -99,3 +145,35 @@ function openImagePopup(cardData) {
   imagePopupCaption.textContent = cardData.name;
   openPopup(largeImagePopup);
 }
+
+enableValidation(validationConfig);
+
+fetch('https://nomoreparties.co/v1/wff-cohort-41/cards', {
+  headers: {
+    authorization: '8d0b0fef-7fc0-4f9e-a81c-c75c92bbe32f'
+  }
+  })
+
+  .then (function(res) {
+    return res.json();
+  })
+  .then  ((data) => {
+    renderCard(data);
+  })
+
+  fetch('https://nomoreparties.co/v1/wff-cohort-41/users/me', {
+  headers: {
+    authorization: '8d0b0fef-7fc0-4f9e-a81c-c75c92bbe32f'
+  }
+  })
+
+  .then (function(res) {
+    return res.json();
+  })
+  .then  ((data) => {
+      profileTitle.textContent = data.name;
+  profileDescription.textContent = data.about;
+  profileImage.style.backgroundImage = `url(${data.avatar})`;
+  })
+
+  
---
+++ src/scripts/validation.js
@@ -0,0 +1,126 @@
+const showInputError = (
+  formElement,
+  inputElement,
+  validationMessage,
+  inputErrorClass,
+  errorClass
+) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(inputErrorClass);
+  errorElement.textContent = validationMessage;
+  errorElement.classList.add(errorClass);
+};
+
+const hideInputError = (
+  formElement,
+  inputElement,
+  inputErrorClass,
+  errorClass
+) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(inputErrorClass);
+  errorElement.classList.remove(errorClass);
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (
+  formElement,
+  inputElement,
+  inputErrorClass,
+  errorClass
+) => {
+  if (!inputElement.validity.valid) {
+    showInputError(
+      formElement,
+      inputElement,
+      inputElement.validationMessage,
+      inputErrorClass,
+      errorClass
+    );
+  } else {
+    hideInputError(formElement, inputElement, inputErrorClass, errorClass);
+  }
+};
+
+const setEventListeners = (
+  formElement,
+  inputSelector,
+  submitButtonSelector,
+  inactiveButtonClass,
+  inputErrorClass,
+  errorClass
+) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+
+  // чтобы проверить состояние кнопки в самом начале
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(
+        formElement,
+        inputElement,
+        inputErrorClass,
+        errorClass
+      );
+      // чтобы проверять его при изменении любого из полей
+      toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+    });
+  });
+};
+
+export function enableValidation(validationConfig) {
+  const formList = Array.from(
+    document.querySelectorAll(validationConfig.formSelector)
+  );
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+    });
+
+    setEventListeners(
+      formElement,
+      validationConfig.inputSelector,
+      validationConfig.submitButtonSelector,
+      validationConfig.inactiveButtonClass,
+      validationConfig.inputErrorClass,
+      validationConfig.errorClass
+    );
+  });
+}
+
+function hasInvalidInput(inputList) {
+  return inputList.some(function (inputElement) {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState(inputList, buttonElement, inactiveButtonClass) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+}
+
+export function clearValidation(formElement, validationConfig) {
+  const inputList = Array.from(
+    formElement.querySelectorAll(validationConfig.inputSelector)
+  );
+  const buttonElement = formElement.querySelector(
+    validationConfig.submitButtonSelector
+  );
+  buttonElement.classList.add(validationConfig.inactiveButtonClass);
+  inputList.forEach((inputElement) => {
+    hideInputError(
+      formElement,
+      inputElement,
+      validationConfig.inputErrorClass,
+      validationConfig.errorClass
+    );
+    inputElement.setCustomValidity("");
+  });
+}
